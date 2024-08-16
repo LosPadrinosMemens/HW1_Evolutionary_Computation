b4b805c8684d836550dc7784498d94bfcdda9496
David Torres Treviño <d.torres.t01@gmail.com>
2024-08-15 23:35:41 -0600

    Sympy added
    Sympy for function expression, evaluation and differentiation.
    
    Hill Climber now is multi objective, and the gradient is almost there I have added the differentiation of nabla and their evaluation. Just missing the matrix multiplication and matrix transverse

--- functions.py
+++ functions.py
@@ -1,5 +1,12 @@
 import random
 import numpy as np
+import sympy as sp
+
+symbols = sp.symbols('x1 x2')
+x1, x2 = sp.symbols('x1 x2')
+f_A = -2*x1**2 + 3*x1*x2 - 1.5*x2**2 - 1.3
+f_B = (4 - 2.1*x1**2 + x1**4/3)*x1**2 + x1*x2 + (-4 + 4*x2**2)*x2**2
+# f_C Rastrigin function, I do not know if 2D or 3D
 
 def obj_function(x):
     """
@@ -7,6 +14,27 @@ def obj_function(x):
     """
     fx = x ** 2 - 40 * x + 50
     return fx
+
+def eval_sympy(obj_func, x):
+    """
+    Parameters:
+    obj_func (sympy exp or list): Objective function or list of derivates
+    x (np array):  Array of values to substitute into the objective function
+
+    Returns:
+    - f(x) (float): The evaluated function value
+    """
+    if isinstance(obj_func, list):
+        return np.array([eval_sympy(func, x) for func in obj_func])
+        
+    elif isinstance(f_A, sp.Expr):
+        sorted_symbols = sorted(obj_func.free_symbols, key=lambda s: s.name)
+        n_x = len(sorted_symbols)
+        if len(x) != n_x:
+            raise ValueError(f"Incompatible dimensions: Expected {n_x} values, but got {len(x)}.")
+        subs_dict = {symbol: value for symbol, value in zip(sorted_symbols, x)}
+        result = obj_func.subs(subs_dict)
+    return float(result)
 
 # Point A
 def gen_neighbor(x_best, delta):
@@ -21,10 +49,10 @@ def gen_neighbor(x_best, delta):
 
 def hill_climber(obj_func, delta, n_iter, x_init = None):
     """
-    Continuous Hill Climber Single Objective Minimization algorithm
+    Continuous Hill Climber Multiple Objective Minimization algorithm
 
     Parameters:
-    - obj_func (func): function that takes an x an returns fx
+    - obj_func (sympy exp): objective function
     - delta (float): open ball radius
     - n_iter (int): number of iterations
     - x_init (float): optional, for initializating the search
@@ -33,12 +61,13 @@ def hill_climber(obj_func, delta, n_iter, x_init = None):
     - x_best: x solution
     - fx_best: f(x)
     """
-    x_best = x_init if x_init is not None else random.randint(1, 100) # Step 1
-    fx_best = obj_func(x_best)
+    n_x = len(obj_func.free_symbols)
+    x_best = x_init if x_init is not None else np.random.randint(1, 101, size=n_x) # Step 1
+    fx_best = eval_sympy(obj_func,x_best)
     i = 0                                                             # Step 2
     while i < n_iter:                                                 # Step 3
         x = gen_neighbor(x_best, delta)                               # Step 4
-        fx = obj_func(x)                          
+        fx = eval_sympy(obj_func,x)                          
         if fx < fx_best:                                              # Step 5
             x_best, fx_best = x, fx                                   # Step 6
             i += 1                                                    # Step 7
@@ -52,30 +81,34 @@ def wolfe_cond(t, x, obj_func, nabla_obj_func, p, c1=0.0001, c2=0.9):
     Parameters:
     - t(float): initial step size
     - x(float): current solution
-    - p(np.ndarray): search direction (from gradient descent)
-    - obj_func (func): function that takes an x an returns fx
-    - nabla_obj_func (np.ndarray): derivative from obj_func
+    - p(np array): search direction (from gradient descent)
+    - obj_func (sympy exp): objective function
+    - nabla_obj_func (sympy exp): derivative from obj_func
 
     Returns:
     - final_t: optimal step size
     """
     flag = False                                                        # Step 3
     while flag is False:                                                # Step 4
-        if obj_func(x+t*p) > obj_func(x) + c1*t*nabla_obj_func(x).T*p:  # Step 5
+        x_tp = x+t*p
+        left_expr1 = eval_sympy(obj_func, x_tp)
+        right_expr1 = eval_sympy(obj_func,x) + c1*t*eval_sympy(nabla_obj_func, x).T*p
+        left_expr2 = eval_sympy(nabla_obj_func, x_tp).T*p
+        right_expr2 = c2*eval_sympy(nabla_obj_func, x).T*p
+        if left_expr1 > right_expr1:                                    # Step 5
             t = t/2                                                     # Step 6
-        elif nabla_obj_func(x+t*p).T*p < c2*nabla_obj_func(x).T*p:      # Step 7
+        elif left_expr2 < right_expr2:                                  # Step 7
             t = t * 2                                                   # Step 8
         else:                                                           # Step 9
             flag = True                                                 # Step 10
     return t                                                            # Step 13
 
-def grad_descent(obj_func, nabla_obj_func, x_init, tol):
+def grad_descent(obj_func, x_init, tol):
     """
     Gradient descent algorithm 
 
     Parameters:
-    - obj_func (func): function that takes an x an returns fx
-    - nabla_obj_func (np.ndarray): derivative from obj_func
+    - obj_func (sympy exp): objective function
     - x_init (np.ndarray): Initialized values
     - tol ()
 
@@ -84,9 +117,15 @@ def grad_descent(obj_func, nabla_obj_func, x_init, tol):
     """
     c = 0                                                                                    # Step 2
     grad_norm = tol + 1                  
-    x_best = x_init                  
+    x_best = x_init
+    sorted_symbols = sorted(obj_func.free_symbols, key=lambda s: s.name)
+    n_x = len(sorted_symbols)
+    nabla_obj_func = list()
+    for i in range(n_x):
+        nabla_obj_func.append(sp.diff(obj_func, sorted_symbols[i]))
+
     while grad_norm > tol:                                                                   # Step 3
-        p = nabla_obj_func(x_best)                                                           # Step 4
+        p = eval_sympy(nabla_obj_func, x_best)                                                           # Step 4
         t = wolfe_cond(t=1, x=x_best, obj_func=obj_func, nabla_obj_func=nabla_obj_func, p=p) # Step 5
         x_best = x_best + t * p                                                              # Step 6
         grad_norm = np.linalg.norm(p)                                                        # Step 7
